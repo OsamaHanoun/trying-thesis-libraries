92c677e4c11aaa52b2d86c7f2a174f0d1bbd94e6
OsamaHanoun <osamahanoun997@gmail.com>
2023-11-01 20:16:01 +0100

    feat(ifc): add LoadingIFCFiles from docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import { useState } from "react";
-import IFC from "./components/IFC/FragmentIfcLoader";
+// import IFC from "./components/IFC/FragmentIfcLoader";
+import IFC from "./components/IFC/LoadingIFCFiles";
+
 import "./App.scss";
 
 function App() {
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { comlink } from "vite-plugin-comlink";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [comlink(), react()],
-  worker: {
-    plugins: [comlink()],
+  plugins: [react()],
+  server: {
+    hmr: false,
   },
 });
